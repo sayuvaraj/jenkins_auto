e6a8a4889fbe4ac435490a9e9db65b384cff6cfb
sayuvaraj <s.a.yuvaraj26@gmail.com>
2024-01-11 12:20:28 +0530

    first

---
+++ src/main/java/sample_group_id/mY_first_artifactid/App.java
@@ -0,0 +1,13 @@
+package sample_group_id.mY_first_artifactid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/sample_group_id/mY_first_artifactid/AppTest.java
@@ -0,0 +1,20 @@
+package sample_group_id.mY_first_artifactid;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
